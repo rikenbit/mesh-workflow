85f3f0987e7dc55c26cf22c2e7bbc5b536138dbf
Manabu ISHII <manabu.ishii.rb@gmail.com>
2022-03-18 22:23:44 +0900

    Update for v003

--- workflow/report.sh
+++ workflow/report.sh
@@ -1,11 +1,11 @@
 # HTML
 mkdir -p report
-mkdir -p report/v002
-snakemake -s workflow/download.smk --report report/v002/download.html
-snakemake -s workflow/ublast.smk --report report/v002/ublast.html
-snakemake -s workflow/preprocess.smk --report report/v002/preprocess.html
-snakemake -s workflow/categorize.smk --report report/v002/categorize.html
-snakemake -s workflow/sqlite.smk --report report/v002/sqlite.html
-snakemake -s workflow/metadata.smk --report report/v002/metadata.html
-snakemake -s workflow/plot.smk --report report/v002/plot.html
+mkdir -p report/v003
+snakemake -s workflow/download.smk --report report/v003/download.html
+snakemake -s workflow/ublast.smk --report report/v003/ublast.html
+snakemake -s workflow/preprocess.smk --report report/v003/preprocess.html
+snakemake -s workflow/categorize.smk --report report/v003/categorize.html
+snakemake -s workflow/sqlite.smk --report report/v003/sqlite.html
+snakemake -s workflow/metadata.smk --report report/v003/metadata.html
+snakemake -s workflow/plot.smk --report report/v003/plot.html
 
