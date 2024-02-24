6556c6bfa20d81b27b1e5171b7b3663c73dd1fa2
Manabu ISHII <manabu.ishii.rb@gmail.com>
2024-02-24 16:59:23 +0900

    v007

--- workflow/report.sh
+++ workflow/report.sh
@@ -1,11 +1,11 @@
 # HTML
 mkdir -p report
-mkdir -p report/v006
-snakemake -s workflow/download.smk --report report/v006/download.html
-snakemake -s workflow/ublast.smk --report report/v006/ublast.html
-snakemake -s workflow/preprocess.smk --report report/v006/preprocess.html
-snakemake -s workflow/categorize.smk --report report/v006/categorize.html
-snakemake -s workflow/sqlite.smk --report report/v006/sqlite.html
-snakemake -s workflow/metadata.smk --report report/v006/metadata.html
-snakemake -s workflow/plot.smk --report report/v006/plot.html
+mkdir -p report/v007
+snakemake -s workflow/download.smk --report report/v007/download.html
+snakemake -s workflow/ublast.smk --report report/v007/ublast.html
+snakemake -s workflow/preprocess.smk --report report/v007/preprocess.html
+snakemake -s workflow/categorize.smk --report report/v007/categorize.html
+snakemake -s workflow/sqlite.smk --report report/v007/sqlite.html
+snakemake -s workflow/metadata.smk --report report/v007/metadata.html
+snakemake -s workflow/plot.smk --report report/v007/plot.html
 
